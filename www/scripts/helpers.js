ab1acbaeccb63cfb17ee5b9c973892890fd43def
Joshua Wilding <joshua_wilding@hotmail.com>
2015-04-30 11:36:36 +0800

    MODIFIED: Fiddled with some of the scripts.

--- www/scripts/helpers.js
+++ www/scripts/helpers.js
@@ -1,73 +1,100 @@
 
 //-----
 
-var cordova_enabled = false;
-var cordova_platform = "";
-var cordova_android = false;
-
-
-function initializeCordova()
+function Exists(value)
 {
-    if (typeof(window.cordova) !== 'undefined')
-    {
-        console.log("USING CORDOVA");
+    return value !== 'undefined';
+};
 
-		cordova_enabled = true;
-        cordova_platform = device.platform.toLowerCase();
-        cordova_android = cordova_platform == "android";
+var phonegap =
+{
+    enabled: false,
+    platform:
+    {
+        id: "",
+        android: false,
+        ios: false,
+        init: function()
+        {
+            console.log("INIT PHONEGAP PLATFORM");
+            this.id = device.platform.toLowerCase();
+            this.android = this.id == "android";
+            this.ios = this.id == "ios";
+        },
+        media: function(src)
+        {
+            if (this.android)
+            {
+                return "file:///android_asset/www/" + src;
+            }
+            return src;
+        }
+    },
+    init: function()
+    {
+        this.enabled = Exists(typeof(window.cordova));
+
+        if (this.enabled)
+        {
+            console.log("INIT PHONEGAP");
+
+            function initPlatform() { phonegap.platform.init(); };
+            document.addEventListener("deviceready", initPlatform, false);
+        }
     }
-}
+};
+
+window.addEventListener('load', function() { phonegap.init() }, false);
+
+
 
 function onReadyEvent(func)
 {
-	function init()
-	{
-		initializeCordova();
-
-		if (cordova_enabled)
-	    {
-	        document.addEventListener("deviceready", func, false);
-	    }
-	    else
-	    {
-	        func();
-	    }
-	};
-	window.addEventListener('load', init, false);
-}
-
-
-//-----
-
-function getMediaURL(s)
-{
-    if (cordova_android)
+    function init()
     {
-        //return "android.resource://au.com.bundiyarra.map/raw/" + s;
-        return "file:///android_asset/www/" + s;
-    }
-    return s;
-}
+        if (phonegap.enabled)
+        {
+            document.addEventListener("deviceready", func, false);
+        }
+        else
+        {
+            func();
+        }
+    };
 
+    window.addEventListener('load', init, false);
+}
 
 
 // Fullscreen video player
 var VideoHelper = function(id)
 {
-    if (cordova_android &&
-        typeof(VideoPlayer) != 'undefined')
+    this.id = id;
+    this.initialize();
+};
+
+onReadyEvent(function()
+{
+    var useMobile = phonegap.enabled;
+    var useVideoPlayer = useMobile && phonegap.platform.android && Exists(typeof(VideoPlayer));
+
+    if (useVideoPlayer)
     {
-        this.play = function (url)
+        console.log("USING VIDEOPLAYER");
+
+        VideoHelper.prototype.initialize = function ()
         {
-            VideoPlayer.play(getMediaURL(url),
+
+        };
+
+        VideoHelper.prototype.play = function (url)
+        {
+            VideoPlayer.play(phonegap.platform.media(url),
                 {
                     volume: 1.0,
-                    scalingMode: VideoPlayer.SCALING_MODE.SCALE_TO_FIT
+                    scalingMode: VideoPlayer.SCALING_MODE.SCALE_TO_FIT_WITH_CROPPING
                 },
-                function ()
-                {
-                    console.log("video completed");
-                },
+                function () { },
                 function (err)
                 {
                     console.log(err);
@@ -75,82 +102,85 @@ var VideoHelper = function(id)
             );
         };
 
-        this.pause = function ()
+        VideoHelper.prototype.pause = function ()
         {
-        	// CANNOT PAUSE
+            // CANNOT PAUSE
         };
 
-        this.stop = function ()
+        VideoHelper.prototype.stop = function ()
         {
             VideoPlayer.Close();
         };
     }
     else
     {
-    	this.id = id;
-
-    	var that = this;
-
-    	var vid = document.createElement("VIDEO");
-    	vid.id = id;
-    	vid.setAttribute("style", 'position: absolute; top: 0; left: 0; right: 0; bottom: 0; width: 100%; height: 100%; background-color: black; z-index: 999; display: none;');
-
-        function showVideo()
+        VideoHelper.prototype.initialize = function ()
         {
-            vid.style.display = "block";
-        }
-        function showVideoPlay()
-        {
-            showVideo();
-            vid.play();
-        }
-
-        function hideVideo()
-        {
-            vid.style.display = "none";
-        }
-        function hideVideoLog(msg)
-        {
-            return function()
+            var createVideoElement = function(id)
             {
-                console.log(msg);
-                hideVideo()
-            };
-        }
+                var vid = document.getElementById(id);
+                if (vid == null)
+                {
+                    vid = document.createElement("VIDEO");
+                    vid.id = id;
+                    vid.setAttribute("style", 'position: absolute; top: 0; left: 0; right: 0; bottom: 0; width: 100%; height: 100%; background-color: black; z-index: 999; display: none;');
+                    document.getElementsByTagName("body")[0].appendChild(vid);
+                }
+                return vid;
+            }
 
-        vid.addEventListener("canplay", showVideoPlay);
-        vid.addEventListener("ended", hideVideo);
-        vid.addEventListener("pause", hideVideo);
-        vid.addEventListener("error", hideVideoLog("error"));
-        vid.addEventListener("stalled", hideVideoLog("stalled"));
+            this.video = createVideoElement(this.id);
 
-        
-        this.play = function (url)
-        {
-            vid.src = url;
-            vid.load();
+            function videoShowHide(msg, show)
+            {
+                return function()
+                {
+                    if (msg != 'undefined') { console.log(msg); }
+                    this.style.display = show ? "block" : "none";
+
+                    if (show)
+                    {
+                        this.play();
+                    }
+                };
+            }
+
+            function showVideo(msg) { return videoShowHide(msg, true); };
+            function hideVideo(msg) { return videoShowHide(msg, false); };
+
+            this.video.addEventListener("canplay", showVideo("canplay"));
+            this.video.addEventListener("ended", hideVideo("ended"));
+            this.video.addEventListener("pause", hideVideo("pause"));
+            this.video.addEventListener("error", hideVideo("error"));
+            this.video.addEventListener("stalled", hideVideo("stalled"));
+
+            var that = this;
+            function stopVideo() { that.stop(); };
+            if (useMobile)
+            {
+                document.addEventListener("backbutton", stopVideo, false);
+            }
+            else
+            {
+                this.video.addEventListener("click", stopVideo);
+            }
         };
 
-        this.pause = function ()
+        VideoHelper.prototype.play = function (url)
         {
-            vid.pause();
+            this.video.src = url;
+            this.video.load();
         };
 
-        this.stop = function ()
+        VideoHelper.prototype.pause = function ()
         {
-            that.pause();
-            hideVideo();
+            this.video.pause();
         };
 
-        if (cordova_android)
+        VideoHelper.prototype.stop = function ()
         {
-            document.addEventListener("backbutton", function() { that.stop(); }, false);
-        }
-        else
-        {
-            vid.addEventListener("click", function() { that.stop(); });
-        }
-
-        document.getElementsByTagName("body")[0].appendChild(vid);
+            this.pause();
+        };
     }
-};
+}
+);
--- www/scripts/index.js
+++ www/scripts/index.js
@@ -4,69 +4,67 @@ var videoPlayer = null;
 
 //-----
 
-var body;
-var outer;
 var main;
 var map;
-
-//-----
-
-var pad = 0;    //PERCENT
-var padding;
-var ratio;
 
 //-----
 
 
 function resizeMap ()
 {
-    //outer.style.width = body.offsetWidth + "px";
-    //outer.style.height = body.offsetHeight + "px";
-
-    var w = main.parentNode.offsetWidth;// - padding;
-    var h = main.parentNode.offsetHeight;// - padding;
-
-    var ratio_parent = w / h;
+    var ratio = map.naturalWidth / map.naturalHeight;
+    var ratio_parent = main.parentNode.offsetWidth / main.parentNode.offsetHeight;
 
     if (ratio_parent > ratio) /* WIDE */
     {
-        //main.style.width = (h * ratio) + "px";
-        //main.style.height = h + "px";
         main.style.width = 100 * ratio / ratio_parent + "%";
         main.style.height = "100%";
     }
     else /* TALL */
     {
-        //main.style.width = w + "px";
-        //main.style.height = (w / ratio) + "px";
         main.style.width = "100%";
-        main.style.height = (w / h * 100) / ratio + "%";
+        main.style.height = 100 * ratio_parent / ratio + "%";
     }
+}
 
-    console.log(main.offsetWidth + ", " + main.offsetHeight);
+function initializeVideoButtons()
+{
+    var bs = document.getElementById('video-buttons');
+    var buttons = bs.getElementsByTagName('div');
+    var imagepath = bs.dataset.imagepath;
+    var imageext = bs.dataset.imageext;
+    var videopath = bs.dataset.videopath;
+    var videoext = bs.dataset.videoext;
+
+    function button_image(id)
+    {
+        return "url(" + imagepath + id + imageext + ")";
+    };
+
+    function button_click()
+    {
+        videoPlayer.play(videopath + this.id + videoext);
+    };
+
+    for (var i = 0; i < buttons.length; i++)
+    {
+        var button = buttons[i];
+        button.style.backgroundImage = button_image(button.id);
+        button.addEventListener("click", button_click);
+    }
 }
 
 function initialize()
 {
-    body = document.getElementsByTagName("body")[0];
-    outer = document.getElementsByClassName('outer')[0];
     main = document.getElementsByClassName('main')[0];
     map = document.getElementById('map');
-    ratio = map.naturalWidth / map.naturalHeight;
-    padding = pad * 2;
-
-    videoPlayer = new VideoHelper("video");
-    var buttons = document.getElementsByClassName('button-video');
-    for (i = 0; i < buttons.length; i++)
-    {
-        buttons[i].addEventListener("click", function()
-        {
-            videoPlayer.play("videos/" + this.id + ".mp4");
-        });
-    }
 
     window.addEventListener("resize", resizeMap);
     resizeMap();
+
+    videoPlayer = new VideoHelper("video");
+
+    initializeVideoButtons();
 }
 
 onReadyEvent(initialize);
